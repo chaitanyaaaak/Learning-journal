9182512b2df0102b7efa25deaed62ecb84f3bb92
Chaitanya Keshniya <chaitanyakeshniya1404@gmail.com>
2025-10-10 23:02:23 +0530

    feat: add DOM element selections

---
+++ script.js
@@ -0,0 +1,16 @@
+import { blogData } from './blogs.js'
+
+const homePage = document.getElementById('home-page');
+const aboutMePage =  document.getElementById('about-me-page');
+const singlePostPage = document.getElementById('single-post-page');
+const heroPage = document.getElementById('hero-page');
+const recentPostsGrid = document.getElementById('recent-posts-grid');  
+const blogGrid = document.getElementById('blog-grid');
+const viewMoreBtn = document.getElementById('view-more-btn');
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body= document.body;
+const logo = document.getElementById('logo-link');
+const home = document.getElementById('home-link');
+const about = document.getElementById('about-me-link');
+
+
